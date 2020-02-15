9f564c577af019da5f633a5f85fe2a83af4bb9d6
DipeshHarijan <dipeshharijan1122@gmail.com>
2020-02-15 16:02:22 +0530

    created login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemFormComponent } from './item-form/item-form.component';
+
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignUpComponent},
+  {path:'add',component:ItemFormComponent},
+  {path:'list',component:ItemsListComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemFormComponent } from './item-form/item-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    SignUpComponent,
+    LoginComponent,
+    ItemsListComponent,
+    ItemDetailsComponent,
+    ItemFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
