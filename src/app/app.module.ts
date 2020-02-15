a58b688d637606d1b5d632ed55cb3bebf52e9c8d
DipeshHarijan <dipeshharijan1122@gmail.com>
2020-02-15 08:12:40 +0530

    emart ui first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemFormComponent } from './item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    SignUpComponent,
+    LoginComponent,
+    ItemsListComponent,
+    ItemDetailsComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule,
